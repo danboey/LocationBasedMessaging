41dac8ffc642438c0d481f4e40520005fea61a56
danboey <dan_boey@hotmail.com>
2014-08-18 23:25:38 +0800

    Update settings.py

--- lbm/settings.py
+++ lbm/settings.py
@@ -1,21 +1,10 @@
 """
 Django settings for lbm project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '8uxrtc942wi=ur!*5p9ysj%-7gf9z!xz^c8vpb@u3uz9_xi)ot'
@@ -54,9 +43,6 @@ ROOT_URLCONF = 'lbm.urls'
 
 WSGI_APPLICATION = 'lbm.wsgi.application'
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
